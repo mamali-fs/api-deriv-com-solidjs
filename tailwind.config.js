90315f35d02cc20e46c19e16c40689a96a4382fc
Ali(Ako) Hosseini <ali.hosseini@firstsource.tech>
2021-11-14 19:14:45 +0330

    feat: add css utilities related to header

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "white-gray":"#c2c2c2"
+      },
+      spacing: {
+        "5%": "5%"
+      }
+    },
   },
   variants: {
     extend: {},
