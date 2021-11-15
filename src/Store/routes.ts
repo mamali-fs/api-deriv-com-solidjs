ec2b5f17c54dfd1587000e1ae5a914551634319a
Ali(Ako) Hosseini <ali.hosseini@firstsource.tech>
2021-11-15 15:50:38 +0330

    add docs subroutes

--- src/Store/routes.ts
+++ src/Store/routes.ts
@@ -4,6 +4,7 @@ import { TNavigation } from './navigation-bar-store';
 const Docs = lazy(() => import('../Pages/Docs/index'));
 const Home = lazy(() => import('../Pages/Home/index'));
 const Playground = lazy(() => import('../Pages/Playground/index'));
+const NotFound = lazy(()=> import("../Pages/NotFound/index"))
 
 export const routes: TNavigation[] = [
   {
@@ -22,3 +23,34 @@ export const routes: TNavigation[] = [
     component: Playground,
   },
 ];
+
+export const docs_routes: TNavigation[]=[{
+  label: 'Quick Start',
+  path: '/docs',
+  component: NotFound,
+},
+  {
+    label: 'App Registration',
+    path: '/docs/app-registration',
+    component: NotFound,
+  },
+  {
+    label: 'API Guide',
+    path: '/docs/api-guide',
+    component: NotFound,
+  },
+  {
+    label: 'FAQ',
+    path: '/docs/faq',
+    component: NotFound,
+  },
+  {
+    label: 'JSON Schemas',
+    path: '/docs/json-schemas',
+    component: NotFound,
+  },
+  {
+    label: 'Bug Bounty',
+    path: '/docs/bug-bounty',
+    component: NotFound,
+  }]
