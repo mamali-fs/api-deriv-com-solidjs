e63317d9189f7d5d8b5f692478c5c50b56b3ef93
Hojjat Jokar <hojjat@firstsource.tech>
2021-11-17 11:29:20 +0330

    fix: styling textarea for request

--- src/Pages/Playground/Request.tsx
+++ src/Pages/Playground/Request.tsx
@@ -3,10 +3,10 @@ import type { Component } from 'solid-js';
 import { createStore } from "solid-js/store";
 
 const defaultReq = {
-    'residence_list': 1
+    residence_list: 1
 }
 
-const Request: Component = ({resetReqs}) => {
+const Request: Component = ({ resetReqs }) => {
     const [fields, setFields] = createStore({ request: defaultReq });
 
     const submit = async () => {
@@ -15,15 +15,19 @@ const Request: Component = ({resetReqs}) => {
 
     return (
         <section className="bg-gray-800 rounded-xl p-4">
+            <label className="text-xs text-gray-400 relative top-4 left-3">Request JSON</label>
             <textarea 
                 placeholder="Request JSON"
-                value={JSON.stringify(fields.request)} 
-                onInput={(e) => setFields("request", e.target.value)}
-                className="w-full h-40 bg-gray-900 text-gray-100 resize-none"
+                value={JSON.stringify(fields.request, null, 2)} 
+                onInput={(e) => {
+                    console.log('e.target.value', e.target.value)
+                    setFields("request", JSON.parse(e.target.value))
+                }}
+                className="w-full h-40 bg-gray-900 text-gray-400 resize-none p-2"
             ></textarea>
             <div className="pt-6 flex justify-center items-center">
                 <button onClick={resetReqs} className="px-4	py-2 rounded-md border-2 border-gray-400 text-white mr-1    ">Reset Connection</button>
-                <button onClick={submit} className="px-4 py-2 rounded-md bg-red-400 border-2 border-red-500 text-white ml-1">Send Request</button>
+                <button onClick={submit} className="px-4 py-2 rounded-md bg-red-500 border-2 border-red-500 text-white ml-1">Send Request</button>
             </div>
         </section>
     )
