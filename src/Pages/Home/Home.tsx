e9e6eb4ff607107381689e31d59fcc87f9503f4e
Adam Rybinski <adam.rybinski@regentmarkets.com>
2021-11-17 16:33:59 +0100

    Add Comprehensive Row to Home Page

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,8 +1,10 @@
 import type { Component } from 'solid-js'
+import ComprehensiveRow from './ComprehensiveRow';
 import Hero from './Hero';
 
 export const Home: Component = () => (
     <div class="flex flex-col overflow-hidden">
         <Hero />
+        <ComprehensiveRow />
     </div>
 )
