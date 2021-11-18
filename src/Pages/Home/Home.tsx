2c2c59c7e65928e17f8ec97638d9a1e58515c44b
Ali(Ako) Hosseini <ali.hosseini@firstsource.tech>
2021-11-18 11:51:51 +0330

    add home slider component

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,5 +1,6 @@
 import type { Component } from 'solid-js'
+import HomeSlider from '../../Components/HomeSlider'
 
 export const Home: Component = () => (
-    <div data-testid="home-page">Home page</div>
+    <div data-testid="home-page"><HomeSlider/></div>
 )
