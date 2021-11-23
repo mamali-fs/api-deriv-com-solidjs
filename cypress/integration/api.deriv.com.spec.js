37602ef0f9617fc387ff6b0cc430f63ba21a5ddb
Hubert Koster <hubertkoster@Huberts-MacBook-Pro.local>
2021-11-23 15:56:15 +0100

    removing comments

--- cypress/integration/api.deriv.com.spec.js
+++ cypress/integration/api.deriv.com.spec.js
@@ -3,23 +3,23 @@
 import { testsModel, testsMobileModel, homeSliderTestsModel } from "./cypressStates";
 import { itTests } from "./itVisitsAndRunsPathTests";
 
-// context("Deriv API full run", () => {
-//   const testPlans = testsModel('home').getShortestPathPlans();
-//   testPlans.forEach((plan) => {
-//     describe(plan.description, () => {
-//       plan.paths.forEach(itTests('/'));
-//     });
-//   });
-// });
+context("Deriv API full run", () => {
+  const testPlans = testsModel('home').getShortestPathPlans();
+  testPlans.forEach((plan) => {
+    describe(plan.description, () => {
+      plan.paths.forEach(itTests('/'));
+    });
+  });
+});
 
-// context("Deriv API mobile", () => {
-//   const testPlans = testsMobileModel('home').getShortestPathPlans();
-//   beforeEach(() => {
-//     // run these tests as if in a mobile browser
-//     // and ensure our responsive UI is correct
-//     cy.viewport('iphone-x')
-//   })
-// });
+context("Deriv API mobile", () => {
+  const testPlans = testsMobileModel('home').getShortestPathPlans();
+  beforeEach(() => {
+    // run these tests as if in a mobile browser
+    // and ensure our responsive UI is correct
+    cy.viewport('iphone-x')
+  })
+});
 
 context("Homepage slider", () => {
   const testPlans = homeSliderTestsModel().getSimplePathPlans();
