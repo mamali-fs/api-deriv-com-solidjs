6aca471d20ec3831236334888af48baee1e4381f
hubert-deriv <80688204+hubert-deriv@users.noreply.github.com>
2021-11-24 10:52:13 +0100

    changing hierarchy and stuff and adding hamburgermachine blueprint (#35)

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -14,7 +14,8 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import './commands';
+import './machine_blueprints/commands.js';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
