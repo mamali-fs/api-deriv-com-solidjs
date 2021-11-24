a493f8dfbf320b00548a8467b6eab1f0df431768
fiona-deriv <76169421+fiona-deriv@users.noreply.github.com>
2021-11-24 12:19:42 +0200

    fiona / add benefits section (#22)
    * add benefits section
    
    * add section margin
    
    Co-authored-by: fiona-deriv <fiona@deriv.com>

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -2,11 +2,13 @@ import type { Component } from 'solid-js'
 import HomeSlider from '../../Components/HomeSlider';
 import ComprehensiveRow from './ComprehensiveRow';
 import Hero from './Hero';
+import Benefits from './Benefits';
 import WaysToEarn from './WaysToEarn'
 
 export const Home: Component = () => (
     <div class="flex flex-col overflow-hidden">
         <Hero />
+        <Benefits />
         <WaysToEarn />
         <HomeSlider />
         <ComprehensiveRow />
